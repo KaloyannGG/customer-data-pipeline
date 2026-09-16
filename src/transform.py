fa6a9d121f6165bb584b1a5c995729d46bc055dc
Kaloyan Georgiev <kaloyangeorgiiev@gmail.com>
2026-09-16 16:09:52 +0300

    Add email duplicate check

--- src/transform.py
+++ src/transform.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 file_path = "data/raw/customers.csv"
 
+seen_emails = []
 #open csv file
 with open(file_path, "r", encoding="utf-8") as file:
 
@@ -13,14 +14,23 @@ with open(file_path, "r", encoding="utf-8") as file:
     email = customer["email"]
     signup_date = customer["signup_date"]
 
+
+
     #check if email is missing
     if email == "":
       print(name, "- Missing email")
     else:
       print(name,"-",email)
 
+    if email != "":    
+      if email in seen_emails:
+        print(name, "- Duplicate customer")
+      else:
+        seen_emails.append(email)
+
+
     #check if the date is valid
-    print("DATE:", signup_date)
+    #print("DATE:", signup_date) -test
     try:
       datetime.strptime(signup_date, "%Y-%m-%d")
     except ValueError:
